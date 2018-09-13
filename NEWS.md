7e056dbc64e94db3f30c13872f5c37fcb91d1373
Jonas Schöley <jschoeley@gmail.com>
2018-09-13 21:50:02 +0000

    version 1.2.0

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,46 @@
+# tricolore 1.2.0
+
+* allow for discrete re-centered scales
+* add new discrete scales TricoloreSextant
+* reorder Tricolore*() arguments
+* rename Tricolore*() list output to `rgb` and `key`
+* add new features to shiny app
+
+# tricolore 1.1.1
+
+* make TernaryLimits() internal
+
+# tricolore 1.1.0
+
+* change defaults
+* make defaults dynamic
+* remove alpha part from rgb codes
+
+# tricolore 1.0.8
+
+* add legend crop option
+* update README
+* add dependencies to travis recipe
+
+# tricolore 1.0.7
+
+* add dependencies to travis recipe
+
+# tricolore 1.0.6
+
+* provide example data as sf data frame
+* use sf in the examples
+* add choropleth maps with tricolore vignette featuring leaflets
+
+# tricolore 1.0.5
+
+* add option for percent-point difference labeling in ternary legend
+* add tests
+
+# tricolore 1.0.4
+
+* establish compatibility with ggplot/ggtern 3.0.0
+
 # tricolore 1.0.3
 
 * Initial CRAN release
--- inst/shiny/app.R
+++ inst/shiny/app.R
@@ -3,16 +3,6 @@ library(dplyr)
 library(ggtern)
 library(tricolore)
 
-# Functions ---------------------------------------------------------------
-
-gghole <- function (fort) {
-  poly <- fort[fort$id %in% fort[fort$hole, ]$id, ]
-  hole <- fort[!fort$id %in% fort[fort$hole, ]$id, ]
-  out <- list(poly, hole)
-  names(out) <- c('poly', 'hole')
-  return(out)
-}
-
 # UI ----------------------------------------------------------------------
 
 ui <- fluidPage(
@@ -23,31 +13,49 @@ ui <- fluidPage(
 
     # INPUT
     sidebarPanel(width = 3,
-                 sliderInput(inputId = 'hue', label = 'Hue', ticks = FALSE,
-                             min = 0, max = 1, step = 0.1, value = 0.3),
-                 sliderInput(inputId = 'chroma', label = 'Chroma', ticks = FALSE,
-                             min = 0, max = 1, step = 0.1, value = 0.9),
-                 sliderInput(inputId = 'lightness', label = 'Lightness', ticks = FALSE,
-                             min = 0, max = 1, step = 0.1, value = 0.8),
-                 sliderInput(inputId = 'contrast', label = 'Contrast', ticks = FALSE,
-                             min = 0, max = 1, step = 0.1, value = 0.6),
-                 sliderInput(inputId = 'spread', label = 'Spread',
-                             min = 0.5, max = 2, step = 0.1, value = 1, ticks = FALSE),
-                 sliderInput(inputId = 'breaks', label = 'Discretization', ticks = FALSE,
-                             min = 2, max = 20, step = 1, value = 5),
-                 radioButtons(inputId = 'center', label = 'Mean centering',
-                              choices = list(No = 'No', Yes = 'Yes'),
-                              selected = 'No'),
-                 radioButtons(inputId = 'show_center', label = 'Show center',
-                              choices = list(No = 'No', Yes = 'Yes'),
-                              selected = 'No'),
-                 radioButtons(inputId = 'show_data', label = 'Show data',
-                              choices = list(No = 'No', Yes = 'Yes'),
-                              selected = 'No')
+                 radioButtons(inputId = 'data', label = 'Data', inline = TRUE,
+                              choices = list('Labour force' = 'lf',
+                                             'Education' = 'educ'),
+                              selected = 'educ'),
+                 radioButtons(inputId = 'type', label = 'Type', inline = TRUE,
+                              choices = list('Default' = 'tricolore',
+                                             'Sextant' = 'sextant'),
+                              selected = 'tricolore'),
+                 conditionalPanel(
+                   condition = 'input.type == "tricolore"',
+                   sliderInput(inputId = 'hue', label = 'Hue', ticks = FALSE,
+                               min = 0, max = 1, step = 0.1, value = 0.2),
+                   sliderInput(inputId = 'chroma', label = 'Chroma', ticks = FALSE,
+                               min = 0, max = 1, step = 0.1, value = 0.7),
+                   sliderInput(inputId = 'lightness', label = 'Lightness', ticks = FALSE,
+                               min = 0, max = 1, step = 0.1, value = 0.8),
+                   sliderInput(inputId = 'contrast', label = 'Contrast', ticks = FALSE,
+                               min = 0, max = 1, step = 0.1, value = 0.4),
+                   sliderInput(inputId = 'spread', label = 'Spread',
+                               min = 0.5, max = 2, step = 0.1, value = 1, ticks = FALSE),
+                 checkboxInput(inputId = 'discrete', label = 'Discrete', value = FALSE),
+                 conditionalPanel(
+                   condition = 'input.discrete',
+                   sliderInput(inputId = 'breaks', label = 'Breaks', ticks = FALSE,
+                               min = 2, max = 20, step = 1, value = 4)
+                 )),
+                 checkboxInput(inputId = 'center', label = 'Mean centering',
+                               value = FALSE),
+                 checkboxInput(inputId = 'show_center', label = 'Show center',
+                               value = FALSE),
+                 checkboxInput(inputId = 'show_data', label = 'Show data',
+                               value = TRUE),
+                 checkboxInput(inputId = 'crop', label = 'Crop legend',
+                               value = FALSE),
+                 radioButtons(inputId = 'label_as', label = 'Label as',
+                              choices = list('percent-share' = 'pct',
+                                             'pct-pt. difference' = 'pct_diff'),
+                              selected = 'pct'),
+                 verbatimTextOutput(outputId = 'call')
     ),
 
     # OUTPUT
-    mainPanel(verbatimTextOutput(outputId = 'call'), plotOutput(outputId = 'example'))
+    mainPanel(plotOutput(outputId = 'example'))
   )
 )
 
@@ -57,57 +65,96 @@ server <- function(input, output) {
 
   output$call <- renderText({
     paste0(
-      "Tricolore(euro_sectors, p1 = 'primary', p2 = 'secondary', p3 = 'tertiary'",
-      ', breaks = ', input$breaks,
-      ', hue = ', input$hue,
-      ', chroma = ', input$chroma,
-      ', lightness = ', input$lightness,
-      ', contrast = ', input$contrast,
-      ', center = ', switch(input$center, No = 'rep(1/3,3)', Yes = 'NA'),
-      ', spread = ', input$spread,
-      ', show_data = ', switch(input$show_data, No = FALSE, Yes = TRUE),
-      ', show_center = ', switch(input$show_center, No = FALSE, Yes = TRUE),
-      ', legend = TRUE)'
+      if (input$type == 'tricolore') 'Tricolore(',
+      if (input$type == 'sextant') 'TricoloreSextant(',
+      "euro_example, ",
+      if (input$data == 'educ') "p1 = 'ed_0to2', p2 = 'ed_3to4', p3 = 'ed_5to8'",
+      if (input$data == 'lf') "p1 = 'lf_pri', p2 = 'lf_sec', p3 = 'lf_ter'",
+      ', center = ', ifelse(input$center, 'NA', 'rep(1/3,3)'),
+      if (input$type == 'tricolore') {
+        paste0(
+          ', breaks = ', ifelse(input$discrete, input$breaks, 'Inf'),
+          ', hue = ', input$hue,
+          ', chroma = ', input$chroma,
+          ', lightness = ', input$lightness,
+          ', contrast = ', input$contrast,
+          ', spread = ', input$spread
+        )
+      },
+      ', legend = TRUE',
+      ', show_data = ', input$show_data,
+      ', show_center = ', input$show_center,
+      ', label_as = "', input$label_as, '"',
+      ', crop = ', input$crop, ')'
     )
   })
 
-  output$example <- renderPlot(width = 800, height = 700, {
+  output$example <- renderPlot(res = 120, width = 1000, height = 800, {
 
-    # mix color, generate legend
-    mixed <- Tricolore(euro_sectors,
-                       p1 = 'primary', p2 = 'secondary', p3 = 'tertiary',
-                       breaks = input$breaks,
-                       hue = input$hue, chroma = input$chroma,
-                       lightness = input$lightness, contrast = input$contrast,
-                       center = switch(input$center, No = rep(1/3,3), Yes = NA),
-                       spread = input$spread,
-                       show_data = switch(input$show_data, No = FALSE, Yes = TRUE),
-                       show_center = switch(input$show_center, No = FALSE, Yes = TRUE),
-                       legend = TRUE)
+    if (input$data == 'educ') {
+      p1 = 'ed_0to2'; p2 = 'ed_3to4'; p3 = 'ed_5to8'
+      title = 'Compos. of education lvls in European regions 2016\n'
+    }
+    if (input$data == 'lf') {
+      p1 = 'lf_pri'; p2 = 'lf_sec'; p3 = 'lf_ter'
+      title = 'Labor force composition in European regions 2016\n'
+    }
+
+    if (input$type == 'tricolore') {
+
+      # mix color, generate legend
+      mixed <- Tricolore(euro_example,
+                         p1 = p1, p2 = p2, p3 = p3,
+                         center = if (input$center) NA else rep(1/3,3),
+                         breaks = ifelse(input$discrete, input$breaks, Inf),
+                         hue = input$hue, chroma = input$chroma,
+                         lightness = input$lightness,
+                         contrast = input$contrast,
+                         spread = input$spread,
+                         show_data = input$show_data,
+                         show_center = input$show_center,
+                         label_as = input$label_as,
+                         crop = input$crop,
+                         legend = TRUE)
+
+    }
+
+    if (input$type == 'sextant') {
+
+      # mix color, generate legend
+      mixed <- TricoloreSextant(euro_example,
+                                p1 = p1, p2 = p2, p3 = p3,
+                                center = if (input$center) NA else rep(1/3,3),
+                                show_data = input$show_data,
+                                show_center = input$show_center,
+                                label_as = input$label_as,
+                                crop = input$crop,
+                                legend = TRUE)
+
+    }
 
     # customize legend
-    lgnd <- mixed[['legend']] +
+    lgnd <- mixed[['key']] +
       labs(x = 'Primary', y = 'Secondary', z = 'Tertiary',
-           caption = paste0('Labor force composition in European regions 2016\n',
-                            switch(input$center, No = 'Colors show deviations from balanced composition',
-                                   input$center, Yes = 'Colors show deviation from average composition'))) +
-      theme(plot.background = element_rect(fill = NA, color = NA))
+           caption = paste0(title,
+                            ifelse(input$center,
+                                   'Colors show deviation from average composition',
+                                   'Colors show deviations from balanced composition'))) +
+      theme(plot.background = element_rect(fill = 'grey95', color = 'grey50'))
 
     # merge data and map
-    euro_sectors$rgb <- mixed[['hexsrgb']]
-    euro_nuts2_sectors <- gghole(right_join(euro_geo_nuts2, euro_sectors, 'id'))
+    euro_example$rgb <- mixed[['rgb']]
 
     # generate map
     euro_map <-
       euro_basemap +
-      geom_polygon(aes(fill = rgb), color = NA,
-                   data = euro_nuts2_sectors$poly) +
-      geom_polygon(aes(fill = rgb), color = NA,
-                   data = euro_nuts2_sectors$hole) +
+      geom_sf(aes(fill = rgb), color = NA,
+              data = euro_example) +
       annotation_custom(ggplotGrob(lgnd),
-                        xmin = 55e5, xmax = Inf,
-                        ymin = 38e5, ymax = Inf) +
+                        xmin = 54e5, xmax = 74e5,
+                        ymin = 8e5, ymax = 80e5) +
       scale_fill_identity() +
+      coord_sf(expand = FALSE, datum = NA) +
       labs(caption = 'Data by eurostat. Jonas Schöley | github.com/jschoeley/tricolore | twitter: @jschoeley')
 
     print(euro_map)
